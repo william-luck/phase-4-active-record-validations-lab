25cc2a1c0c45e5e675271087fb6b38b6798beeab
William Luck <luck.william.h@gmail.com>
2022-11-04 11:09:55 -0300

    commit tree 589ae3aac7a5a461a5f353a3b5f3fcd621b06b82

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :title, :click_bait
+
+    def click_bait
+
+        
+        accepted_values = ["Won't Believe", "Secret", "Top", "Guess"]
+
+        if title
+            unless accepted_values.any? {|word| title.include?(word)}
+            errors.add(:title, "Not click bait enough")
+            end
+        end
+
+        
+
+
+
+        # byebug
+
+    end
 end
